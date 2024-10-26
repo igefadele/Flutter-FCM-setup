fb881c6acc3828d4a1338c95789d9b1b54751d89
amanullahgit <amanullah.askari@gmail.com>
2024-10-26 10:54:01 +0530

    foreground & background commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:fcm_test/services/notification_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await NotificationService.instance.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('hello'),
+        ),
+      ),
+    );
+  }
+}
